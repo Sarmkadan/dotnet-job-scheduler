f927119b54e79e70a6157a69c15101a380a0223b
Vladyslav Zaiets <rutova2@gmail.com>
2020-09-24 13:19:32 +0300

    feat(dotnet-job-scheduelr.tests): more JobPipelineService extension meth

---
+++ tests/dotnet-job-scheduler.Tests/JobPipelineServiceTestsExtensions.cs
@@ -0,0 +1,126 @@
+#nullable enable
+
+using FluentAssertions;
+using JobScheduler.Core.Data;
+using JobScheduler.Core.Domain.Entities;
+using JobScheduler.Core.Domain.Models;
+using JobScheduler.Core.Exceptions;
+using JobScheduler.Core.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace DotnetJobScheduler.Tests;
+
+/// <summary>
+/// Extension methods for <see cref="JobPipelineServiceTests"/> that provide reusable test utilities
+/// for testing <see cref="JobPipelineService"/> functionality.
+/// </summary>
+public static class JobPipelineServiceTestsExtensions
+{
+    /// <summary>
+    /// Creates a test service with in-memory database context and mocked dependencies.
+    /// </summary>
+    /// <param name="test">The test instance.</param>
+    /// <param name="depServiceMock">Optional mock for <see cref="IJobDependencyService"/>. If null, a default mock is created.</param>
+    /// <returns>A tuple containing the service and its context.</returns>
+    public static (JobPipelineService Service, JobSchedulerContext Context) CreateService(
+        this JobPipelineServiceTests test,
+        Mock<IJobDependencyService>? depServiceMock = null)
+    {
+        var ctx = CreateInMemoryContext();
+        depServiceMock ??= new Mock<IJobDependencyService>();
+
+        depServiceMock
+            .Setup(d => d.AddDependencyAsync(
+                It.IsAny<Guid>(),
+                It.IsAny<Guid>(),
+                It.IsAny<string?>(),
+                It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        depServiceMock
+            .Setup(d => d.RemoveDependencyAsync(
+                It.IsAny<Guid>(),
+                It.IsAny<Guid>(),
+                It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        var service = new JobPipelineService(ctx, depServiceMock.Object);
+        return (service, ctx);
+    }
+
+    /// <summary>
+    /// Creates a simple job for testing.
+    /// </summary>
+    /// <param name="test">The test instance.</param>
+    /// <param name="name">The job name.</param>
+    /// <returns>A configured <see cref="Job"/> entity.</returns>
+    public static Job CreateJob(
+        this JobPipelineServiceTests test,
+        string name)
+    {
+        return new Job
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            CronExpression = "0 9 * * *",
+            HandlerType = "TestApp.TestJob, TestApp",
+            MaxRetries = 3,
+            ExecutionTimeoutSeconds = 300,
+            MaxConcurrentExecutions = 1
+        };
+    }
+
+    /// <summary>
+    /// Creates a test pipeline with the specified name and steps.
+    /// </summary>
+    /// <param name="service">The pipeline service.</param>
+    /// <param name="name">The pipeline name.</param>
+    /// <param name="steps">The pipeline steps.</param>
+    /// <param name="createdBy">The creator identifier. Defaults to "test-user".</param>
+    /// <returns>The created pipeline.</returns>
+    public static async Task<JobPipeline> CreateTestPipelineAsync(
+        this JobPipelineService service,
+        string name,
+        IReadOnlyList<PipelineStepRequest> steps,
+        string createdBy = "test-user")
+    {
+        var request = new CreatePipelineRequest
+        {
+            Name = name,
+            Description = "Test pipeline",
+            Steps = steps.ToList()
+        };
+
+        return await service.CreatePipelineAsync(request, createdBy);
+    }
+
+    /// <summary>
+    /// Asserts that a pipeline has the expected steps in the correct order.
+    /// </summary>
+    /// <param name="pipeline">The pipeline to verify.</param>
+    /// <param name="expectedStepNames">The expected job names in order.</param>
+    public static void ShouldHaveStepsInOrder(
+        this JobPipeline pipeline,
+        IReadOnlyList<string> expectedStepNames)
+    {
+        pipeline.Should().NotBeNull();
+        pipeline.Steps.Should().HaveCount(expectedStepNames.Count);
+
+        for (var i = 0; i < expectedStepNames.Count; i++)
+        {
+            pipeline.Steps[i].StepOrder.Should().Be(i);
+            pipeline.Steps[i].Job.Should().NotBeNull();
+            pipeline.Steps[i].Job!.Name.Should().Be(expectedStepNames[i]);
+        }
+    }
+
+    private static JobSchedulerContext CreateInMemoryContext()
+    {
+        var options = new DbContextOptionsBuilder<JobSchedulerContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        return new JobSchedulerContext(options);
+    }
+}
