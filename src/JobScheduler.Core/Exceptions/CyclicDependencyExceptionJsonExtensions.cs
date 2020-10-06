1b86a907e885c885249c4a047d281bf251f61d10
Vladyslav Zaiets <rutova2@gmail.com>
2020-10-06 10:26:01 +0300

    Json serialization for CyclicDependencyExceptionJson

---
+++ src/JobScheduler.Core/Exceptions/CyclicDependencyExceptionJsonExtensions.cs
@@ -0,0 +1,82 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+
+namespace JobScheduler.Core.Exceptions;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="CyclicDependencyException"/>.
+/// </summary>
+public static class CyclicDependencyExceptionJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="CyclicDependencyException"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The exception to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the exception.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is null.</exception>
+    public static string ToJson(this CyclicDependencyException value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="CyclicDependencyException"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized exception, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown if the JSON is malformed or cannot be deserialized.</exception>
+    public static CyclicDependencyException? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<CyclicDependencyException>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="CyclicDependencyException"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized exception if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out CyclicDependencyException? value)
+    {
+        value = null;
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return false;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<CyclicDependencyException>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
