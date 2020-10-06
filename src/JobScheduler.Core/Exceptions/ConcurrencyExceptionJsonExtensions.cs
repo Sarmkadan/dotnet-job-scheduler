5191b719fc42490c9c743fc3229d3947b068d711
Vladyslav Zaiets <rutova2@gmail.com>
2020-10-06 08:08:25 +0300

    json round-trip support for ConcurrencyExceptionJson
    will circle back to tests later.

---
+++ src/JobScheduler.Core/Exceptions/ConcurrencyExceptionJsonExtensions.cs
@@ -0,0 +1,85 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace JobScheduler.Core.Exceptions;
+
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="ConcurrencyException"/>.
+/// </summary>
+public static class ConcurrencyExceptionJsonExtensions
+{
+    private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes the specified <see cref="ConcurrencyException"/> to a JSON string.
+    /// </summary>
+    /// <param name="value">The exception to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the exception.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this ConcurrencyException value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_options) { WriteIndented = true }
+            : _options;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="ConcurrencyException"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized exception, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ConcurrencyException? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ConcurrencyException>(json, _options);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="ConcurrencyException"/> from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized exception if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out ConcurrencyException? value)
+    {
+        value = null;
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<ConcurrencyException>(json, _options);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
