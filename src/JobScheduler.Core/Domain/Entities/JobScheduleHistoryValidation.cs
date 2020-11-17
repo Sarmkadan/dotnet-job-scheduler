bd6031ae91bd422e838536a6ad8220d023f640fc
Vladyslav Zaiets <rutova2@gmail.com>
2020-11-17 16:49:25 +0200

    Input validation for JobScheduleHistory

--- src/JobScheduler.Core/Domain/Entities/JobScheduleHistoryValidation.cs
+++ src/JobScheduler.Core/Domain/Entities/JobScheduleHistoryValidation.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 
 namespace JobScheduler.Core.Domain.Entities
 {
+    /// <summary>
+    /// Provides validation methods for <see cref="JobScheduleHistory"/> instances.
+    /// </summary>
     public static class JobScheduleHistoryValidation
     {
+        /// <summary>
+        /// Validates a <see cref="JobScheduleHistory"/> instance and returns a list of validation errors.
+        /// </summary>
+        /// <param name="value">The job schedule history instance to validate.</param>
+        /// <returns>A read-only list of validation error messages. Empty if validation succeeds.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
         public static IReadOnlyList<string> Validate(this JobScheduleHistory value)
         {
-            var problems = new List<string>();
+            ArgumentNullException.ThrowIfNull(value);
 
-            if (value == null)
-            {
-                problems.Add("JobScheduleHistory instance cannot be null.");
-                return problems;
-            }
+            var problems = new List<string>();
 
             if (value.Id == Guid.Empty)
             {
@@ -40,16 +45,34 @@ namespace JobScheduler.Core.Domain.Entities
                 problems.Add("ChangedAt cannot be the default value.");
             }
 
-            return problems;
+            return problems.AsReadOnly();
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="JobScheduleHistory"/> instance is valid.
+        /// </summary>
+        /// <param name="value">The job schedule history instance to check.</param>
+        /// <returns><see langword="true"/> if the instance is valid; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
         public static bool IsValid(this JobScheduleHistory value)
         {
-            return value.Validate().Count == 0;
+            ArgumentNullException.ThrowIfNull(value);
+
+            return !string.IsNullOrWhiteSpace(value.PropertyName)
+                   && value.JobId != Guid.Empty
+                   && !string.IsNullOrWhiteSpace(value.ChangeReason);
         }
 
+        /// <summary>
+        /// Validates the specified <see cref="JobScheduleHistory"/> instance and throws an exception if validation fails.
+        /// </summary>
+        /// <param name="value">The job schedule history instance to validate.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
+        /// <exception cref="ArgumentException">Thrown when the instance fails validation.</exception>
         public static void EnsureValid(this JobScheduleHistory value)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
             var errors = value.Validate();
             if (errors.Count > 0)
             {
