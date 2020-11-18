4abba0535a508c23ad676fe4a78db6798e76066f
Vladyslav Zaiets <rutova2@gmail.com>
2020-11-18 08:03:00 +0200

    Add extension helpers for JobExecutionSummary.

--- src/JobScheduler.Core/Domain/Models/JobExecutionSummaryExtensions.cs
+++ src/JobScheduler.Core/Domain/Models/JobExecutionSummaryExtensions.cs
@@ -5,6 +5,7 @@
 // CTO & Software Architect
 // =============================================================================
 
+using System;
 using JobScheduler.Core.Domain.Models;
 
 namespace JobScheduler.Core.Domain.Models;
@@ -19,40 +20,42 @@ public static class JobExecutionSummaryExtensions
     /// Calculates the failure rate percentage for the job execution.
     /// </summary>
     /// <param name="summary">The job execution summary to analyze.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="summary"/> is <see langword="null"/>.</exception>
     /// <returns>Failure rate as percentage (0-100), or 0 if no executions.</returns>
     public static double GetFailureRate(this JobExecutionSummary summary)
     {
-        if (summary.TotalExecutions == 0)
-        {
-            return 0;
-        }
+        ArgumentNullException.ThrowIfNull(summary);
 
-        return (double)summary.FailureCount / summary.TotalExecutions * 100;
+        return summary.TotalExecutions == 0
+            ? 0
+            : (double)summary.FailureCount / summary.TotalExecutions * 100;
     }
 
     /// <summary>
     /// Calculates the percentage of executions that were either timed out or cancelled.
     /// </summary>
     /// <param name="summary">The job execution summary to analyze.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="summary"/> is <see langword="null"/>.</exception>
     /// <returns>Timeout/cancelled rate as percentage (0-100), or 0 if no executions.</returns>
     public static double GetTimeoutCancelledRate(this JobExecutionSummary summary)
     {
-        if (summary.TotalExecutions == 0)
-        {
-            return 0;
-        }
+        ArgumentNullException.ThrowIfNull(summary);
 
-        var timeoutCancelledCount = summary.TimedOutCount + summary.CancelledCount;
-        return (double)timeoutCancelledCount / summary.TotalExecutions * 100;
+        return summary.TotalExecutions == 0
+            ? 0
+            : (double)(summary.TimedOutCount + summary.CancelledCount) / summary.TotalExecutions * 100;
     }
 
     /// <summary>
     /// Determines if the job execution has any failures (failed, timed out, or cancelled).
     /// </summary>
     /// <param name="summary">The job execution summary to check.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="summary"/> is <see langword="null"/>.</exception>
     /// <returns>True if there are any failures; otherwise, false.</returns>
     public static bool HasFailures(this JobExecutionSummary summary)
     {
+        ArgumentNullException.ThrowIfNull(summary);
+
         return summary.FailureCount > 0 || summary.TimedOutCount > 0 || summary.CancelledCount > 0;
     }
 
@@ -60,20 +63,26 @@ public static class JobExecutionSummaryExtensions
     /// Gets the duration range (min to max) for job executions.
     /// </summary>
     /// <param name="summary">The job execution summary to analyze.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="summary"/> is <see langword="null"/>.</exception>
     /// <returns>A tuple containing (MinDurationMs, MaxDurationMs).</returns>
     public static (long Min, long Max) GetDurationRange(this JobExecutionSummary summary)
     {
+        ArgumentNullException.ThrowIfNull(summary);
+
         return (summary.MinDurationMs, summary.MaxDurationMs);
     }
 
     /// <summary>
-    /// Calculates the standard deviation of execution durations (simplified estimation).
-    /// Uses the range-based approximation: stdDev ≈ (max - min) / 6 for normal distributions.
+    /// Calculates the standard deviation of execution durations using a range-based approximation.
+    /// Uses the formula: stdDev ≈ (max - min) / 6 for normal distributions.
     /// </summary>
     /// <param name="summary">The job execution summary to analyze.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="summary"/> is <see langword="null"/>.</exception>
     /// <returns>Estimated standard deviation of execution durations in milliseconds.</returns>
     public static double GetDurationStandardDeviation(this JobExecutionSummary summary)
     {
+        ArgumentNullException.ThrowIfNull(summary);
+
         if (summary.TotalExecutions == 0 || summary.MinDurationMs == summary.MaxDurationMs)
         {
             return 0;
