d167fcaf5ed3c49a1ba1922434b10294b2ca7832
Vladyslav Zaiets <rutova2@gmail.com>
2020-11-30 12:59:09 +0200

    Tidy up ScheduleServiceTests

--- tests/dotnet-job-scheduler.Tests/ScheduleServiceTests.cs
+++ tests/dotnet-job-scheduler.Tests/ScheduleServiceTests.cs
@@ -11,18 +11,32 @@ using Xunit;
 
 namespace DotnetJobScheduler.Tests;
 
+/// <summary>
+/// Unit tests for <see cref="ScheduleService"/> functionality.
+/// Tests the service methods that handle job scheduling, execution time calculation,
+/// and cron expression processing.
+/// </summary>
 public sealed class ScheduleServiceTests
 {
     private readonly Mock<IJobRepository> _jobRepoMock = new();
     private readonly Mock<CronExpressionService> _cronServiceMock = new();
     private readonly Mock<ILogger<ScheduleService>> _loggerMock = new();
 
-    private ScheduleService CreateService() => new(
+    /// <summary>
+/// Creates a new instance of <see cref="ScheduleService"/> for testing.
+/// </summary>
+/// <returns>A configured <see cref="ScheduleService"/> instance with mock dependencies.</returns>
+private ScheduleService CreateService() => new(
         _jobRepoMock.Object,
         _cronServiceMock.Object,
         _loggerMock.Object);
 
-    private static Job CreateJob(Guid? id = null) => new()
+    /// <summary>
+/// Creates a test job entity with default values.
+/// </summary>
+/// <param name="id">Optional job ID. If null, generates a new GUID.</param>
+/// <returns>A configured <see cref="Job"/> instance for testing purposes.</returns>
+private static Job CreateJob(Guid? id = null) => new()
     {
         Id = id ?? Guid.NewGuid(),
         Name = "test-job",
@@ -36,7 +50,11 @@ public sealed class ScheduleServiceTests
     };
 
     [Fact]
-    public async Task GetUpcomingExecutionTimesAsync_WithValidJob_ReturnsMultipleTimes()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetUpcomingExecutionTimesAsync"/> returns multiple upcoming execution times
+	/// for a valid, active job.
+	/// </summary>
+	public async Task GetUpcomingExecutionTimesAsync_WithValidJob_ReturnsMultipleTimes()
     {
         // Arrange
         var jobId = Guid.NewGuid();
@@ -62,7 +80,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetUpcomingExecutionTimesAsync_WithInactiveJob_ReturnsEmpty()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetUpcomingExecutionTimesAsync"/> returns empty collection
+	/// when the job exists but is marked as inactive.
+	/// </summary>
+	public async Task GetUpcomingExecutionTimesAsync_WithInactiveJob_ReturnsEmpty()
     {
         // Arrange
         var jobId = Guid.NewGuid();
@@ -81,7 +103,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetUpcomingExecutionTimesAsync_WithNonexistentJob_ReturnsEmpty()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetUpcomingExecutionTimesAsync"/> returns empty collection
+	/// when the job does not exist in the repository.
+	/// </summary>
+	public async Task GetUpcomingExecutionTimesAsync_WithNonexistentJob_ReturnsEmpty()
     {
         // Arrange
         _jobRepoMock.Setup(r => r.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Job?)null);
@@ -96,7 +122,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetUpcomingExecutionTimesAsync_RespectsCronExpression()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetUpcomingExecutionTimesAsync"/> correctly respects the cron expression
+	/// and returns execution times that match the specified schedule.
+	/// </summary>
+	public async Task GetUpcomingExecutionTimesAsync_RespectsCronExpression()
     {
         // Arrange
         var jobId = Guid.NewGuid();
@@ -126,7 +156,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetUpcomingExecutionTimesAsync_WithDefaultCount_Returns10Times()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetUpcomingExecutionTimesAsync"/> returns 10 execution times by default
+	/// when no count parameter is specified.
+	/// </summary>
+	public async Task GetUpcomingExecutionTimesAsync_WithDefaultCount_Returns10Times()
     {
         // Arrange
         var jobId = Guid.NewGuid();
@@ -146,7 +180,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetExecutionFrequencyPerDayAsync_CalculatesCorrectly()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetExecutionFrequencyPerDayAsync"/> correctly calculates the number of times a cron expression
+	/// will execute in a single day.
+	/// </summary>
+	public async Task GetExecutionFrequencyPerDayAsync_CalculatesCorrectly()
     {
         // Arrange
         var cronExpression = "0 * * * *"; // Hourly
@@ -170,7 +208,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetExecutionFrequencyPerDayAsync_WithDailySchedule_ReturnsOne()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetExecutionFrequencyPerDayAsync"/> returns 1 for a daily cron expression
+	/// that executes once per day at midnight.
+	/// </summary>
+	public async Task GetExecutionFrequencyPerDayAsync_WithDailySchedule_ReturnsOne()
     {
         // Arrange
         var cronExpression = "0 0 * * *"; // Daily at midnight
@@ -191,7 +233,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetExecutionFrequencyPerDayAsync_WithEveryFiveMinutes_ReturnsCorrectCount()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetExecutionFrequencyPerDayAsync"/> correctly calculates the frequency for an expression
+	/// that runs every 5 minutes (288 times per day).
+	/// </summary>
+	public async Task GetExecutionFrequencyPerDayAsync_WithEveryFiveMinutes_ReturnsCorrectCount()
     {
         // Arrange
         var cronExpression = "*/5 * * * *"; // Every 5 minutes
@@ -215,7 +261,11 @@ public sealed class ScheduleServiceTests
     }
 
     [Fact]
-    public async Task GetExecutionFrequencyPerDayAsync_HandlesException_ReturnsZero()
+    	/// <summary>
+	/// Tests that <see cref="ScheduleService.GetExecutionFrequencyPerDayAsync"/> gracefully handles invalid cron expressions
+	/// and returns 0 instead of throwing exceptions.
+	/// </summary>
+	public async Task GetExecutionFrequencyPerDayAsync_HandlesException_ReturnsZero()
     {
         // Arrange
         var cronExpression = "invalid cron";
