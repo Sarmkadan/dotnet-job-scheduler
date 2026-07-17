cbe07755f2d749809fd0f94548fddcb9fa1f81f9
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 15:10:00 +0300

    refactor: nits
    will circle back to tests later.

--- src/JobScheduler.Core/Domain/Entities/ExecutionMetricsExtensions.cs
+++ src/JobScheduler.Core/Domain/Entities/ExecutionMetricsExtensions.cs
@@ -13,9 +13,13 @@ public static class ExecutionMetricsExtensions
     /// <param name="minimumExecutions">The minimum number of executions required.</param>
     /// <returns>True if the job is consistently reliable; otherwise, false.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="metrics"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="minimumExecutions"/> is negative or <paramref name="minimumSuccessRatePercent"/> is outside the range [0, 100].</exception>
     public static bool IsConsistentlyReliable(this ExecutionMetrics metrics, double minimumSuccessRatePercent = 90.0, int minimumExecutions = 10)
     {
         ArgumentNullException.ThrowIfNull(metrics);
+        ArgumentOutOfRangeException.ThrowIfLessThan(minimumExecutions, 0);
+        ArgumentOutOfRangeException.ThrowIfLessThan(minimumSuccessRatePercent, 0.0);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(minimumSuccessRatePercent, 100.0);
 
         return metrics.IsReliable(minimumSuccessRatePercent) && metrics.TotalExecutions >= minimumExecutions;
     }
@@ -40,9 +44,12 @@ public static class ExecutionMetricsExtensions
     /// <param name="thresholdFailureRate">The threshold failure rate.</param>
     /// <returns>True if the job has a high failure rate; otherwise, false.</returns>
     /// <exception cref="ArgumentNullException">Thrown if <paramref name="metrics"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="thresholdFailureRate"/> is outside the range [0, 1].</exception>
     public static bool HasHighFailureRate(this ExecutionMetrics metrics, double thresholdFailureRate = 0.1)
     {
         ArgumentNullException.ThrowIfNull(metrics);
+        ArgumentOutOfRangeException.ThrowIfLessThan(thresholdFailureRate, 0.0);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(thresholdFailureRate, 1.0);
 
         return 1 - metrics.SuccessRate >= thresholdFailureRate;
     }
