ddece6a1e8c3e356835ba9b619edc5f3ce195768
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Cleanup in DailySalesReportJobHandlerJsonExtensions

--- examples/DailySalesReportJobHandlerJsonExtensions.cs
+++ examples/DailySalesReportJobHandlerJsonExtensions.cs
@@ -13,50 +13,46 @@ using System.Text.Json;
 /// </summary>
 public static class DailySalesReportJobHandlerJsonExtensions
 {
-    private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
     {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
     };
 
     /// <summary>
     /// Serializes the <see cref="DailySalesReportJobHandler"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The job handler instance to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability</param>
-    /// <returns>A JSON string representation of the job handler</returns>
-    /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+    /// <param name="value">The job handler instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the job handler.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this DailySalesReportJobHandler value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
-            ? new JsonSerializerOptions(_options) { WriteIndented = true }
-            : _options;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+        => JsonSerializer.Serialize(
+            ArgumentNullException.ThrowIfNull(value),
+            indented
+                ? new JsonSerializerOptions(_options) { WriteIndented = true }
+                : _options);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="DailySalesReportJobHandler"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <returns>The deserialized job handler instance, or null if deserialization fails</returns>
-    /// <exception cref="ArgumentNullException">Thrown when json is null</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized</exception>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized job handler instance, or <see langword="null"/> if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
+    /// <exception cref="JsonException"><paramref name="json"/> is invalid or cannot be deserialized to <see cref="DailySalesReportJobHandler"/>.</exception>
     public static DailySalesReportJobHandler? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
-
         return JsonSerializer.Deserialize<DailySalesReportJobHandler>(json, _options);
     }
 
     /// <summary>
     /// Attempts to deserialize a JSON string to a <see cref="DailySalesReportJobHandler"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <param name="value">Receives the deserialized job handler instance if successful</param>
-    /// <returns>True if deserialization succeeds; false otherwise</returns>
-    /// <exception cref="ArgumentNullException">Thrown when json is null</exception>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized job handler instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out DailySalesReportJobHandler? value)
     {
         ArgumentNullException.ThrowIfNull(json);
