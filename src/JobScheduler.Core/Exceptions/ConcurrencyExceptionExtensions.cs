c577eea9fb280ed3a8849fdc21084fec8cb38e57
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 11:12:37 +0300

    Dedpue
    found this while looking at something else.

--- src/JobScheduler.Core/Exceptions/ConcurrencyExceptionExtensions.cs
+++ src/JobScheduler.Core/Exceptions/ConcurrencyExceptionExtensions.cs
@@ -6,8 +6,8 @@ public static class ConcurrencyExceptionExtensions
     /// Determines whether the concurrency exception indicates that the job is currently running at maximum allowed concurrency.
     /// </summary>
     /// <param name="exception">The concurrency exception to check.</param>
-    /// <returns>true if the job is currently running at maximum allowed concurrency; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns><see langword="true"/> if the job is currently running at maximum allowed concurrency; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static bool IsAtMaxConcurrency(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
@@ -18,8 +18,8 @@ public static class ConcurrencyExceptionExtensions
     /// Calculates the number of additional executions that can be started without exceeding the maximum allowed concurrency.
     /// </summary>
     /// <param name="exception">The concurrency exception to check.</param>
-    /// <returns>The number of additional executions that can be started.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+    /// <returns>The number of additional executions that can be started. Returns 0 if current executions exceed maximum.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is <see langword="null"/>.</exception>
     public static int GetAvailableConcurrencySlots(this ConcurrencyException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
