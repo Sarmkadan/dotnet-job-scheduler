f95b5cafde813c8d31e90225cd3dfee592e4401c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 11:48:52 +0300

    Dedupe some logic in JobValidationExceptionExtensions.

--- src/JobScheduler.Core/Exceptions/JobValidationExceptionExtensions.cs
+++ src/JobScheduler.Core/Exceptions/JobValidationExceptionExtensions.cs
@@ -10,27 +10,23 @@ public static class JobValidationExceptionExtensions
     /// </summary>
     /// <param name="exception">The <see cref="JobValidationException"/> instance.</param>
     /// <returns>A formatted error message.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
     public static string GetFormattedMessage(this JobValidationException exception)
-    {
-        ArgumentNullException.ThrowIfNull(exception);
-
-        return string.IsNullOrEmpty(exception.PropertyName)
+        => string.IsNullOrEmpty(exception?.PropertyName) is true
             ? exception.Message
             : $"Validation error on {exception.PropertyName}: {exception.Message}";
-    }
 
     /// <summary>
     /// Creates a new <see cref="JobValidationException"/> with the same message and property name.
     /// </summary>
     /// <param name="exception">The <see cref="JobValidationException"/> instance.</param>
     /// <returns>A new <see cref="JobValidationException"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown if <paramref name="exception"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
     public static JobValidationException Clone(this JobValidationException exception)
     {
         ArgumentNullException.ThrowIfNull(exception);
 
-        return new JobValidationException(exception.Message)
+        return new(exception.Message)
         {
             PropertyName = exception.PropertyName,
         };
