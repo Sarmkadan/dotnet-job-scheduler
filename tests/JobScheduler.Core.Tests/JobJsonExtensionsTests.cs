dd437a322e99569c9cac71098b46177f8e59b7a3
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    chore(jobscheduler.core.tests): update JobJsonExtensionsTests
    also touched a couple of call sites.

---
+++ tests/JobScheduler.Core.Tests/JobJsonExtensionsTests.cs
@@ -0,0 +1,10 @@
+// tests/JobScheduler.Core.Tests/JobJsonExtensionsTests.cs
+
+using Xunit;
+
+namespace JobScheduler.Core.Tests;
+
+public class JobJsonExtensionsTests
+{
+    // ... test methods ...
+}
