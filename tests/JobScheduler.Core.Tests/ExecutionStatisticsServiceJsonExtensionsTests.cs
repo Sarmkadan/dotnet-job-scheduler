e8b22613047192f155b969d06c0857caaa672286
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 06:55:01 +0300

    chore(jobscheduler.core.tests): minor
    also touched a couple of call sites.

---
+++ tests/JobScheduler.Core.Tests/ExecutionStatisticsServiceJsonExtensionsTests.cs
@@ -0,0 +1,51 @@
+using JobScheduler.Core.Services;
+using JobScheduler.Core.Tests;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JobScheduler.Core.Tests
+{
+    public class ExecutionStatisticsServiceJsonExtensionsTests
+    {
+        [Fact]
+        public void ToJson_Happy_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void FromJson_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TryFromJson_HAPPY_PATH_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToJson_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void FromJson_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TryFromJson_NULL_INPUT_Test()
+        {
+            // Test implementation here
+        }
+    }
+}
