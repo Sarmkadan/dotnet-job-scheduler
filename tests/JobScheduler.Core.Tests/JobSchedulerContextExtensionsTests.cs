651ac375250b320ab9620d02c221baa30914751c
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 20:26:38 +0300

    Tweak JobSchedulerContextExtensionsTests

---
+++ tests/JobScheduler.Core.Tests/JobSchedulerContextExtensionsTests.cs
@@ -0,0 +1,58 @@
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+namespace JobScheduler.Core.Tests
+{
+    public class JobSchedulerContextExtensionsTests
+    {
+        [Fact]
+        public async Task TestFindNextExecutableJobAsync_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestFindNextExecutableJobAsync_InvalidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetJobsWithRecentExecutionsAsync_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetJobsWithRecentExecutionsAsync_InvalidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestCreateJobExecutionAsync_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestCreateJobExecutionAsync_InvalidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetJobExecutionStatsAsync_ValidInput()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetJobExecutionStatsAsync_InvalidInput()
+        {
+            // Test implementation
+        }
+    }
+}
