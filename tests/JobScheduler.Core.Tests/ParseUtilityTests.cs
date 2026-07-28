efb71e73bf0845ba92787b42e5fdad4d18a6151b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 13:49:32 +0300

    chore: tweak ParseUtilityTests

---
+++ tests/JobScheduler.Core.Tests/ParseUtilityTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace JobScheduler.Core.Tests
+{
+    public class ParseUtilityTests
+    {
+        [Fact]
+        public void ParseInt_HappyPath_ValidInput_ParsesCorrectly()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void ParseInt_HappyPath_InvalidInput_ThrowsFormatException()
+        {
+            // Test implementation
+        }
+    }
+}
