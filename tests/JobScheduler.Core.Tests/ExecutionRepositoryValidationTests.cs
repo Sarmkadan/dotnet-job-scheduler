bdf1657a65eb26487c71b8519675a69e3dd671f0
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 13:01:48 +0300

    fix(jobscheduler.core.tests): fix build in ExecutionRepositoryValidation

---
+++ tests/JobScheduler.Core.Tests/ExecutionRepositoryValidationTests.cs
@@ -0,0 +1,40 @@
+using Xunit;
+using JobScheduler.Core.Data.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JobScheduler.Core.Tests
+{
+    public class ExecutionRepositoryValidationTests
+    {
+        [Fact]
+        public void Validate_Happy_Path()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValid_Happy_Path()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EnsureValid_Happy_Path()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void Validate_Empty_Collection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValid_Null_Input()
+        {
+            // Test implementation here
+        }
+    }
+}
