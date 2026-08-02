be1e53a753e007f6ac8b76ab8229c57c13b57abc
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-02 19:56:44 +0000

    chore: apply workspace updates

---
+++ tests/JobScheduler.Core.Tests/DistributedJobLockServiceExtensionsTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using JobScheduler.Core.Services;
+using System.Threading.Tasks;
+using Moq;
+
+namespace JobScheduler.Core.Tests
+{
+    public class DistributedJobLockServiceExtensionsTests
+    {
+        [Fact]
+        public async Task TestTryAcquireLockWithRetryAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestExecuteWithLockAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestIsHeldByAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetRemainingLockTimeAsync()
+        {
+            // Test implementation
+        }
+    }
+}
