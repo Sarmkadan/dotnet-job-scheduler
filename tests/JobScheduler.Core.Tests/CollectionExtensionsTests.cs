6a932dc4390ff8d3b463e18e171e5e23e58a80bf
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-04 13:26:14 +0000

    chore: apply workspace updates

---
+++ tests/JobScheduler.Core.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using JobScheduler.Core.Extensions;
+using JobScheduler.Core.Tests;
+
+namespace JobScheduler.Core.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        // Test methods go here
+    }
+}
