a929fe46317657b8cd9ef1fa34ded128ffc12d14
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-10 05:51:48 +0000

    docs: add XML documentation to IRepository interface

--- src/JobScheduler.Core/Data/Repositories/IRepository.cs
+++ src/JobScheduler.Core/Data/Repositories/IRepository.cs
@@ -17,17 +17,88 @@ namespace JobScheduler.Core.Data.Repositories;
 /// <typeparam name="T">The entity type</typeparam>
 public interface IRepository<T> where T : class
 {
+    /// <summary>
+    /// Gets an entity by its ID asynchronously.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity.</param>
+    /// <returns>The entity if found, otherwise null.</returns>
     Task<T?> GetByIdAsync(Guid id);
+
+    /// <summary>
+    /// Gets all entities asynchronously.
+    /// </summary>
+    /// <returns>A collection of all entities.</returns>
     Task<IEnumerable<T>> GetAllAsync();
+
+    /// <summary>
+    /// Finds entities matching the given predicate asynchronously.
+    /// </summary>
+    /// <param name="predicate">The predicate to filter entities.</param>
+    /// <returns>A collection of entities matching the predicate.</returns>
     Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Gets the first entity matching the given predicate, or default if none found, asynchronously.
+    /// </summary>
+    /// <param name="predicate">The predicate to filter entities.</param>
+    /// <returns>The first entity matching the predicate, or null if none found.</returns>
     Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Counts the number of entities, optionally filtered by a predicate, asynchronously.
+    /// </summary>
+    /// <param name="predicate">The predicate to filter entities (optional).</param>
+    /// <returns>The number of entities matching the predicate, or total count if predicate is null.</returns>
     Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+
+    /// <summary>
+    /// Adds a new entity asynchronously.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task AddAsync(T entity);
+
+    /// <summary>
+    /// Adds a range of entities asynchronously.
+    /// </summary>
+    /// <param name="entities">The collection of entities to add.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task AddRangeAsync(IEnumerable<T> entities);
+
+    /// <summary>
+    /// Updates an existing entity.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
     void Update(T entity);
+
+    /// <summary>
+    /// Updates a range of entities.
+    /// </summary>
+    /// <param name="entities">The collection of entities to update.</param>
     void UpdateRange(IEnumerable<T> entities);
+
+    /// <summary>
+    /// Removes an entity.
+    /// </summary>
+    /// <param name="entity">The entity to remove.</param>
     void Remove(T entity);
+
+    /// <summary>
+    /// Removes a range of entities.
+    /// </summary>
+    /// <param name="entities">The collection of entities to remove.</param>
     void RemoveRange(IEnumerable<T> entities);
+
+    /// <summary>
+    /// Determines whether any entity matches the given predicate asynchronously.
+    /// </summary>
+    /// <param name="predicate">The predicate to test entities.</param>
+    /// <returns>True if any entity matches the predicate; otherwise, false.</returns>
     Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Saves all changes made in the repository asynchronously.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task SaveChangesAsync();
 }
